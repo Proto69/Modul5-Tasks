8873cf6dd33dadcf544eb9218ff492043a4e7ee0
Proto68 <timkaq67@gmail.com>
2022-10-11 18:16:27 +0300

    06

---
+++ FROG/Lake.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FROG
+{
+    internal class Lake : IEnumerable<int>
+    {
+        public List<int> List { get; set; }
+        private int index;
+
+        public Lake(List<int> list)
+        {
+            List = list;
+            this.index = 0;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return List[i];
+                }
+            }
+            for (int i = List.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return List[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ FROG/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace FROG
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<int> input = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToList();
+            Lake lake = new(input);
+
+            foreach (var item in lake)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
