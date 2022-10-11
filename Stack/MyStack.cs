3f924f98be43e49bb40950070825d70cf5642e74
Proto68 <timkaq67@gmail.com>
2022-10-11 18:04:51 +0300

    06

---
+++ Stack/MyStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> list;
+
+        public MyStack()
+        {
+            this.list = new();
+        }
+
+        public void Push(List<T> items)
+        {
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+        public T Pop()
+        {
+            if (list.Count == 0)
+                throw new IndexOutOfRangeException("The stack is empty!");
+            T item = list[list.Count - 1];
+            list.Remove(item);
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = list.Count - 1; i >= 0 ; i--)
+            {
+                yield return list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Stack/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace Stack
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            MyStack<string> stack = new();
+            while ((list = Console.ReadLine().Split().ToList())[0].ToUpper() != "END")
+            {
+                try
+                {
+                    switch (list[0])
+                    {
+                        case "Push":
+                            List<string> inputs = new List<string>();
+                            for (int i = 1; i < list.Count; i++)
+                            {
+                                inputs.Add(list[i]);
+                            }
+                            if (inputs.Count != 0)
+                                stack.Push(inputs);
+                            break;
+                        case "Pop":
+                            stack.Pop();
+                            break;
+                    }
+                }
+                catch (Exception k)
+                {
+                    Console.WriteLine(k.Message);
+                }
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Main();
+        }
+    }
+}
