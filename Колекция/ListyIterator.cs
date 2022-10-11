55548b0d3da0d17edccaf82673ed269daa161601
Proto68 <timkaq67@gmail.com>
2022-10-11 17:46:32 +0300

    06

---
+++ Колекция/ListyIterator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        public List<T> List { get; set; }
+        public int Index { get; set; }
+
+        public ListyIterator(List<T> list)
+        {
+            this.List = list;
+            this.Index = 0;
+        }
+        public ListyIterator() : this(new List<T>())
+        {
+        }
+
+        public bool Move()
+        {
+            Index++;
+            return HasNext();
+        }
+        public bool HasNext()
+        {
+            if (Index >= List.Count)
+                return false;
+            return true;
+        }
+        public void Print()
+        {
+            CheckIfEmpty();
+            Console.WriteLine(List[Index]);
+        }
+        public void PrintAll()
+        {
+            CheckIfEmpty();
+            string result = "";
+            foreach (var item in List)
+            {
+                result += $"{item} ";
+            }
+            Console.WriteLine(result);
+        }
+
+        private bool CheckIfEmpty()
+        {
+            if (List.Count == 0 || !HasNext())
+                throw new ArgumentException("Invalid operation!");
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < List.Count; i++)
+            {
+                yield return List[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Колекция/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace ListyIterator
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            ListyIterator<string> iterator = new ListyIterator<string>();
+            while ((list = Console.ReadLine().Split().ToList())[0].ToUpper() != "END")
+            {
+                try
+                {
+                    switch (list[0])
+                    {
+                        case "Create":
+                            List<string> inputs = new List<string>();
+                            for (int i = 1; i < list.Count; i++)
+                            {
+                                inputs.Add(list[i]);
+                            }
+                            if (inputs.Count != 0)
+                                iterator = new ListyIterator<string>(inputs);
+                            break;
+                        case "HasNext":
+                            Console.WriteLine(iterator.HasNext());
+                            break;
+                        case "Move":
+                            Console.WriteLine(iterator.Move());
+                            break;
+                        case "Print":
+                            iterator.Print();
+                            break;
+                        case "PrintAll":
+                            iterator.PrintAll();
+                            break;
+                    }
+                }
+                catch (Exception k)
+                {
+                    Console.WriteLine(k.Message);
+                }
+            }
+            Main();
+        }
+    }
+}
