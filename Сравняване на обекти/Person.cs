4171b5fe190635732163d49d42367a6109af8cfb
Proto68 <timkaq67@gmail.com>
2022-10-15 20:59:23 +0300

    06

---
+++ Сравняване на обекти/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сравняване_на_обекти
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            string name = other.Name;
+            int age = other.Age;
+            string town = other.Town;
+            if (name == this.Name && age == this.Age && town == this.Town)
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
---
+++ Сравняване на обекти/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace Сравняване_на_обекти
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> input = new();
+            List<Person> persons = new();
+            while ((input = Console.ReadLine().Split().ToList())[0].ToUpper() != "END")
+            {
+                Person person = new(input[0], int.Parse(input[1]), input[2]);
+                persons.Add(person);
+            }
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            foreach (Person person in persons)
+            {
+                if (person.CompareTo(persons[n - 1]) == 0)
+                    count++;
+            }
+            if (count == 1)
+                Console.WriteLine("No matches");
+            else
+                Console.WriteLine($"{count} {persons.Count - count} {persons.Count}");
+        }
+    }
+}
