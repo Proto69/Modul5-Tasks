876820bd197cd24ecbd7a58dd0312557b2f5d27a
Proto68 <timkaq67@gmail.com>
2022-10-19 17:54:17 +0300

    10

---
+++ Четене на редове/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Четене_на_редове
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Testing
+            StreamReader streamReader = new("smth.txt");
+            string? line = streamReader.ReadLine();
+            line = streamReader.ReadLine();
+            line = streamReader.ReadLine();
+            line = streamReader.ReadLine();
+            line = streamReader.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+}
