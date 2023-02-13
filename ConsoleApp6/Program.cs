8ed83f28f8c00271a0727038cd79acb24651f259
Proto68 <timkaq67@gmail.com>
2023-02-13 15:44:02 +0200

    ...

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -4,21 +4,32 @@ namespace ConsoleApp6
 {
     internal class Program
     {
+        public static string connectionString = $"{server};{database};{uid};{pwd}";
+        private static string server = "Server=sql.freedb.tech";
+        private static string database = "Database=freedb_PlanMe_DB";
+        private static string uid = "Uid=freedb_PlanMe";
+        private static string pwd = "Pwd=etkN3V6sZ3Pv&CU";
+
         static void Main(string[] args)
         {
             MySqlConnection conn = 
                 new("Server=sql.freedb.tech;Database=freedb_PlanMe_DB;Uid=freedb_PlanMe;Pwd=etkN3V6sZ3Pv&CU");
+            string phrase = "";
+            Random random = new Random();
+            int randomNum = random.Next(0, 390);
             conn.Open();
             using (conn)
             {
-                MySqlCommand cmd = new(
-                    "INSERT INTO users(username, password)" +
-                    "VALUES (@name, @password);"
-                    , conn);
-                cmd.Parameters.AddWithValue("@name", "Pesho");
-                cmd.Parameters.AddWithValue("@password", "123456");
-                cmd.ExecuteNonQuery();
+                string query = "SELECT phrase FROM daily_phrase WHERE id = @id";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", randomNum);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                reader.Read();
+                
+                    phrase = reader["phrase"].ToString();
+                
             }
+            Console.WriteLine(phrase);
         }
     }
 }
